28b46c754de5da003b26e6ae8aa2a1082af858da
Tarzan <tarzan@MacBook-Air-2.local>
2019-10-18 16:52:09 +0200

    jsps are now separated by functions with include statements

--- EShop/WebContent/error.jsp
+++ EShop/WebContent/error.jsp
@@ -1,12 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+    pageEncoding="UTF-8" import="java.util.*, es.uc3m.eshop.model.*"%>
+
+<jsp:include page="header.jsp"/>
 Opps!!! Sorry!!!
-</body>
-</html>
+<jsp:include page="footer.jsp"/>
--- EShop/src/es/uc3m/eshop/handler/IndexRequestHandler.java
+++ EShop/src/es/uc3m/eshop/handler/IndexRequestHandler.java
@@ -15,10 +15,13 @@ public class IndexRequestHandler implements es.uc3m.eshop.controlservlet.Request
 			throws ServletException, IOException {
 		
 		
+		
+		
+		
 		List<Product> list = new ArrayList<Product>();
 		list.add(new Product(1,"Television","great television",300.0f,"image1.png"));
-		list.add(new Product(1,"Mobile","buy it",800.0f,"image1.png"));
-		list.add(new Product(1,"TV2","also a television",100.0f,"image1.png"));
+		list.add(new Product(1,"Smart Phone","buy it",800.0f,"image1.png"));
+		list.add(new Product(1,"Keyboard","also a television",30.0f,"image1.png"));
 		list.add(new Product(1,"Computer","yeah, buy this computer",600.0f,"image1.png"));
 		list.add(new Product(1,"Pen","The greatest pen",3.0f,"image1.png"));
 		

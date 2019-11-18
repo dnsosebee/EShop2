f4a8e43123fb11b1b4da6e55f6eee04ab5671d34
Rees <reespawson@gmail.com>
2019-11-18 17:52:38 +0100

    Removed Old DBImage code

--- EShop/src/es/uc3m/eshop/handler/AddProductHandler.java
+++ EShop/src/es/uc3m/eshop/handler/AddProductHandler.java
@@ -2,7 +2,6 @@ package es.uc3m.eshop.handler;
 
 import java.io.IOException;
 
-import es.uc3m.eshop.imagemanagement.DBImage;
 import es.uc3m.eshop.model.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -39,39 +38,27 @@ public class AddProductHandler implements es.uc3m.eshop.controlservlet.RequestHa
 
 		if (request.getParameter("action").equalsIgnoreCase("addProduct")) {
 
-			System.out.println("READY TO ADD THE NEW PRODUCT");
 			// Product details
-			
 			String seller = au.getEmail();
 			String productName = request.getParameter("productName");
 			String productDescription = request.getParameter("productDescription");
 			float productPrice = Float.parseFloat(request.getParameter("productPrice"));
 			int productStock = Integer.parseInt(request.getParameter("productStock"));
 
-			  //Adding the image
-			  DBImage productImage = new DBImage();
-			  Part inImage = request.getPart("productImage");
-			  byte[] imageData = new byte[(int) inImage.getSize()]; 
-			  inImage.getInputStream().read(imageData, 0 ,imageData.length);
-			  
-			/*
-			 * productImage.setImage(data); productImage.setTitle(productName);
-			 */
-			  
-			  
-			  
-			  
-			  
-			  Product addProduct = new Product();
-			  addProduct.setDescription(productDescription);
-			  addProduct.setName(productName); addProduct.setSeller(seller);
-			  addProduct.setPrice(productPrice); addProduct.setStock(productStock);
-			  addProduct.setImage(imageData);
-			  
-			  
-			  
-			  pm.insert(addProduct);
-			 
+			// Adding the image
+			Part inImage = request.getPart("productImage");
+			byte[] imageData = new byte[(int) inImage.getSize()];
+			inImage.getInputStream().read(imageData, 0, imageData.length);
+
+			Product addProduct = new Product();
+			addProduct.setDescription(productDescription);
+			addProduct.setName(productName);
+			addProduct.setSeller(seller);
+			addProduct.setPrice(productPrice);
+			addProduct.setStock(productStock);
+			addProduct.setImage(imageData);
+
+			pm.insert(addProduct);
 
 			return "products.html";
 

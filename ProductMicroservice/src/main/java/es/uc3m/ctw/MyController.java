78976779bf36b0a2411654a604810263b6625ca9
Daniel Sosebee <sosebee@wustl.edu>
2019-12-11 09:58:48 +0100

    product microservice complete, minus keyword search

--- ProductMicroservice/src/main/java/es/uc3m/ctw/MyController.java
+++ ProductMicroservice/src/main/java/es/uc3m/ctw/MyController.java
@@ -21,16 +21,17 @@ public class MyController {
 
 	@Autowired
 	ProductDAO productDAO;
-	
-	@RequestMapping(value="products", method=RequestMethod.GET)
-	public @ResponseBody ResponseEntity<List<Product>> findAll(@RequestParam(required = false) Double min, @RequestParam(required = false) Double max) {
-		
-		List<Product> list = productDAO.custom(min, max);
-		
+
+	@RequestMapping(value = "products", method = RequestMethod.GET)
+	public @ResponseBody ResponseEntity<List<Product>> findAll(@RequestParam(required = false) Double min,
+			@RequestParam(required = false) Double max, @RequestParam(required = false) String seller) {
+
+		List<Product> list = productDAO.custom(min, max, seller);
+
 		return new ResponseEntity<List<Product>>(list, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value="products/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "products/{id}", method = RequestMethod.GET)
 	public @ResponseBody ResponseEntity<?> findById(@PathVariable int id) {
 		Product product = productDAO.findById(id).orElse(null);
 		if (product == null) {
@@ -38,8 +39,8 @@ public class MyController {
 		}
 		return new ResponseEntity<Product>(product, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value="products", method=RequestMethod.POST)
+
+	@RequestMapping(value = "products", method = RequestMethod.POST)
 	public @ResponseBody ResponseEntity<?> save(@RequestBody Product product) {
 		if (productDAO.existsById(product.getIdProduct())) {
 			return new ResponseEntity<>(HttpStatus.CONFLICT);
@@ -47,4 +48,31 @@ public class MyController {
 		Product savedProduct = productDAO.save(product);
 		return new ResponseEntity<Product>(savedProduct, HttpStatus.CREATED);
 	}
+	
+	@RequestMapping(value = "products", method = RequestMethod.PUT)
+	public @ResponseBody ResponseEntity<?> update(@RequestBody Product product) {
+		Product dbProduct = productDAO.findById(product.getIdProduct()).orElse(null);
+		if (dbProduct == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+		}
+		dbProduct.setDescription(product.getDescription());
+		dbProduct.setName(product.getName());
+		dbProduct.setPrice(product.getPrice());
+		dbProduct.setProductImage(product.getProductImage());
+		dbProduct.setSeller(product.getSeller());
+		dbProduct.setStock(product.getStock());
+		productDAO.save(dbProduct);
+		return new ResponseEntity<Product>(dbProduct, HttpStatus.OK);
+	}
+		
+	@RequestMapping(value = "products/{id}", method = RequestMethod.DELETE)
+	public @ResponseBody ResponseEntity<?> delete(@PathVariable int id) {
+		Product product = productDAO.findById(id).orElse(null);
+		if (product == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+		}
+		productDAO.delete(product);
+		return new ResponseEntity<Product>(HttpStatus.OK);
+	}
+		
 }
--- ProductMicroservice/src/main/java/es/uc3m/ctw/model/ProductDAO.java
+++ ProductMicroservice/src/main/java/es/uc3m/ctw/model/ProductDAO.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductDAO extends CrudRepository<Product,Integer> {
- 
+public interface ProductDAO extends CrudRepository<Product, Integer> {
+
 	List<Product> findAll();
-	
-	@Query("Select p from Product p where (:min is null OR p.price > :min) AND (:max is null OR p.price < :max)")
-	List<Product> custom(Double min, Double max);
-	
-	
+
+	@Query("Select p from Product p where (:min is null OR p.price > :min) AND (:max is null OR p.price < :max) AND (:seller is null OR p.seller = :seller)")
+	List<Product> custom(Double min, Double max, String seller);
 }
